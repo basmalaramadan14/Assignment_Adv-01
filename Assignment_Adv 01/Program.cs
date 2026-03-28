4b859cb3bb69d6b5c7fa55c887b457a636d7f9a8
Basmala Ramadan <basmalaramadan51@gmail.com>
2026-03-29 00:30:05 +0200

    Add project files.

---
+++ Assignment_Adv 01/Program.cs
@@ -0,0 +1,16 @@
+﻿namespace Assignment_Adv_01
+{
+    #region Q01
+    //A generic class uses type parameters that are replaced with actual types when you create an instance.
+    //The type parameter T acts as a placeholder.
+    //---
+
+    #endregion
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
