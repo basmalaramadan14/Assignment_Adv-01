3eae777a8625e6d0d10da1d03fb5201076bf159a
Basmala Ramadan <basmalaramadan51@gmail.com>
2026-03-29 00:40:58 +0200

    Q01

--- Assignment_Adv 01/Program.cs
+++ Assignment_Adv 01/Program.cs
@@ -3,7 +3,10 @@
     #region Q01
     //A generic class uses type parameters that are replaced with actual types when you create an instance.
     //The type parameter T acts as a placeholder.
-    //---
+    //-------//
+    //We use generics to make classes, methods,
+    //and interfaces work with different data types while maintaining type safety
+    //and code reusability.
 
     #endregion
     internal class Program
