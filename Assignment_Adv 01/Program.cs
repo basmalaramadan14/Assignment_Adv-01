bde7baff0a96170b3d2fbce78818ddaf587c07e7
Basmala Ramadan <basmalaramadan51@gmail.com>
2026-03-29 03:10:13 +0200

    Q09

--- Assignment_Adv 01/Program.cs
+++ Assignment_Adv 01/Program.cs
@@ -90,7 +90,39 @@
     //    public T Value { get; set; }
     //}
     #endregion
-
+    #region Q09
+    //T must have parameterless constructor 
+    //------//
+    //public class Factory<T> where T : new()
+    //{
+    //    public T Create()
+    //    {
+    //        return new T();
+    //    }
+    //}
+    #endregion
+    #region 10
+    #endregion
+    #region 11
+    #endregion
+    #region 12
+    #endregion
+    #region 13
+    #endregion
+    #region 14
+    #endregion
+    #region 15
+    #endregion
+    #region 16
+    #endregion
+    #region 17
+    #endregion
+    #region 18
+    #endregion
+    #region 19
+    #endregion
+    #region 20
+    #endregion
     internal class Program
     {
         //static void Main(string[] args)
